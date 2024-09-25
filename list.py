2d7a3429b04fc0593fac127ab92aa3770cc6440b
BlobbaFett <quebecmc100@gmail.com>
2024-09-25 14:03:58 -0600

    coso nuevo

---
+++ list.py
@@ -0,0 +1,71 @@
+#Homogeneous list
+integers = [1, 2, 3, 4, 5]
+
+animals = ['cat', 'elephant', 'dog'  ]
+
+names = ['Michelle', 'Poller', 'Anahi']
+
+floats = [2.3, 4.5, 8.9 ]
+
+#Heterogeneous list
+number_animals_floats_names = [2, 'cat', 33.34, 'Deriva']
+
+list_lists= [[1,2,3], ['cat', 'dog', 'dragon']]
+
+#How to access an element in the list 
+random_words = [
+    "apple", "giraffe", "ocean", "whisper", "quantum",
+    "serendipity", "jigsaw", "horizon", "mosaic", "telescope",
+    "lighthouse", "kaleidoscope", "dragonfly", "paradox", "nebula",
+    "stardust", "cascade", "phantom", "rendezvous", "labyrinth",
+    "sky", "cloud", "mountain", "river", "forest",
+    "firefly", "whirlwind", "echo", "symphony", "dandelion",
+    "enigma", "journey", "voyage", "twilight", "harmony",
+    "illusion", "spectacle", "gravity", "sapphire", "ethereal",
+    "dreamcatcher", "crystal", "sunrise", "moonlight", "petal",
+    "glimmer", "mystery", "cosmos", "reflection", "solstice",
+    "serenade", "traverse", "breeze", "ember", "fable",
+    "whirl", "canvas", "pulse", "velocity", "alchemy",
+    "sundown", "raindrop", "melody", "seeker", "wanderlust",
+    "whimsy", "paradise", "sphinx", "tapestry", "anomaly",
+    "flare", "catalyst", "crescendo", "bloom", "veil",
+    "seraph", "epiphany", "mirage", "odyssey", "glisten",
+    "soliloquy", "eclipse", "frost", "spectrum", "cynosure",
+    "quasar", "kismet", "lullaby", "reverie", "quilt",
+    "tides", "siren", "quicksilver", "sundance", "horizon",
+    "ripple", "whistle", "tide", "vortex", "tornado",
+    "crimson", "flame", "wisp", "nexus", "cascades",
+    "incandescent", "tranquil", "enlightenment", "chroma", "radiance",
+    "harbinger", "moonbeam", "celestial", "vagabond", "synthesis",
+    "sublime", "phoenix", "zenith", "luminary", "adventure",
+    "haven", "evergreen", "iridescence", "shadow", "panorama",
+    "gleam", "arcadia", "dream", "sanctuary", "reflections",
+    "ephemeral", "rift", "gleam", "crystalline", "nostalgia",
+    "serenity", "dusk", "reflection", "ambrosia", "utopia",
+    "galaxy", "infinity", "euphoria", "crescent", "vortex",
+    "mystique", "radiate", "gleaming", "luminescence", "whirl",
+    "ethos", "spectrum", "timbre", "kaleidoscope", "haze",
+    "sanctum", "melancholy", "apogee", "valiant", "verdant",
+    "tether", "cascade", "mirage", "bastion", "eclipse",
+    "aura", "gossamer", "silhouette", "tryst", "ciphers",
+    "interlude", "mirth", "bliss", "eclipse", "phantasm",
+    "scepter", "neophyte", "seraphim", "talisman", "epoch",
+    "destiny", "meadow", "soulmate", "enchantment", "whimsical",
+    "phenomenon", "embrace", "galore", "whirlwind", "cipher",
+    "stardust", "whistle", "solstice", "flicker", "mystic",
+    "dreamscape", "radiance", "canvas", "allure", "cherub",
+    "paragon", "evanescent", "brilliance", "blossom", "tide",
+    "serenity", "odyssey", "expanse", "whirl", "utopia"
+]
+
+print(random_words[-1])
+
+#list slicing
+list = [1,2,3,4,5,6,7,8,9,10]
+list.pop
+print(list[0:-1])
+
+#update list
+science = ['Biology', 'Math', 'art']
+science [2] = 'geology' 
+print(science)
