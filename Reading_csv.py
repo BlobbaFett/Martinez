a8b74fd7302c52396ca7f166929b6c07d894346b
BlobbaFett <quebecmc100@gmail.com>
2024-11-12 14:16:15 -0600

    Me la rife

--- Reading_csv.py
+++ Reading_csv.py
@@ -22,40 +22,42 @@ def read_csv(file_name):
         return mortalkombat
 
 def display_info(year_info):
-
-        print("Menu:")
-        print("1. Fatalities")
-        print("2. Injuries")
-        print("3. Crashes")
-        print('4. Fatal Crashes')
-        print("4. Distraction affected fatal crashes")
-        print("5. Fatal crashes involving cell phone use")
-        print('6. Fatal crashes involving excessive speed')
-        print('7. Fatal Crashes while driving under the influence')
-        print('8. Fatal Crashes involving fatigue or illness:')
-        
-        choice = input("Enter your choice: ")
-        
-        if choice == '1':
-            print(f'Fatalities: {year_info[0]}')
-        elif choice == '2':
-            print(f'Injuries: {year_info[1]}')
-        elif choice == '3':
-            print(f'Crashes: {year_info[2]}')
-        elif choice == '4':            
-            print(f'Fatal crashes: {year_info[3]}')
-        elif choice == '5':  
-            print(f'Distraction affected fatal crashes: {year_info[4]}')
-        elif choice =='6':           
-            print(f'Fatal crashes involving cell phone use: {year_info [5]}')
-        elif choice == '7':
-            print(f'Fatal crashes involving excessive speed: {year_info[6]}')
-        elif choice == '8':
-            print(f'Fatal Crashes while driving under the influence: {year_info[7]}')
-        elif choice =='9':
-            print(f'Fatal Crashes involving fatigue or illness: {year_info[8]}')
+        if year_info:
+            print("Menu:")
+            print("1. Fatalities")
+            print("2. Injuries")
+            print("3. Crashes")
+            print('4. Fatal Crashes')
+            print("4. Distraction affected fatal crashes")
+            print("5. Fatal crashes involving cell phone use")
+            print('6. Fatal crashes involving excessive speed')
+            print('7. Fatal Crashes while driving under the influence')
+            print('8. Fatal Crashes involving fatigue or illness:')
+            
+            choice = input("Enter your choice: ")
+            
+            if choice == '1':
+                print(f'Fatalities: {year_info[0]}')
+            elif choice == '2':
+                print(f'Injuries: {year_info[1]}')
+            elif choice == '3':
+                print(f'Crashes: {year_info[2]}')
+            elif choice == '4':            
+                print(f'Fatal crashes: {year_info[3]}')
+            elif choice == '5':  
+                print(f'Distraction affected fatal crashes: {year_info[4]}')
+            elif choice =='6':           
+                print(f'Fatal crashes involving cell phone use: {year_info [5]}')
+            elif choice == '7':
+                print(f'Fatal crashes involving excessive speed: {year_info[6]}')
+            elif choice == '8':
+                print(f'Fatal Crashes while driving under the influence: {year_info[7]}')
+            elif choice =='9':
+                print(f'Fatal Crashes involving fatigue or illness: {year_info[8]}')
+            else:
+                print('Invalid choice')
         else:
-            print('Invalid choice')
+            print('Year not found')
 def main():
     file_name = r'C:\Users\Labinfo10\Documents\Quebec Martinez\archivos de chamba\accidents.csv'
     
